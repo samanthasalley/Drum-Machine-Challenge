2649bf13c53663f1d1dcbee6ce9c06af466d6427
Samantha Salley <samanthasalley@gmail.com>
2019-05-22 15:59:31 -0700

    (solution) sams version of solution

--- src/components/app.js
+++ src/components/app.js
@@ -12,13 +12,24 @@ class App extends Component {
 
   playSound(eventObj) {
     // code to be run when click event is fired goes below this line!
+    const soundToPlay = Sounds[eventObj.currentTarget.id];
+    soundToPlay.currentTime = 0;
+    soundToPlay.play();
   }
 
   render() {
+    const buttons = [];
+    for (let i = 0; i < this.state.sounds.length; i++) {
+      buttons.push((<Button
+        key={i}
+        sound={this.state.sounds[i]}
+        playSound={this.playSound}
+      />));
+    }
     return (
       <div className="button-container">
         {/* Components that need to be returned from App go below here ! */}
-        
+        {buttons}
       </div>
     );
   }
@@ -29,6 +40,7 @@ class Button extends Component {
     return (
       <div
         // Add additional attributes here
+        id={this.props.sound}
         className="button"
         onClick={this.props.playSound}
       >
